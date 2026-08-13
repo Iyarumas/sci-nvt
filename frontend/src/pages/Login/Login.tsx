86e4699d6d399cc499860992a50c860e29c8b9f6
Iyarumas <gui.cardias@gmail.com>
2026-08-12 21:30:28 -0300

    testes

--- frontend/src/pages/Login/Login.tsx
+++ frontend/src/pages/Login/Login.tsx
@@ -25,7 +25,7 @@ export function Login() {
   async function handleSubmit(e: React.FormEvent) {
     e.preventDefault();
     if (!username || !password) {
-      setError('Preencha todos os campossssssss.');
+      setError('Preencha todos os campos.');
       return;
     }
     setError('');
